1de37e00552fb5046897a05c5d75b7c52b4218f4
cmr119 <cryan@nearinfinity.com>
2012-12-13 11:13:20 -0500

    Finish user edit, update, index and destroy actions (Ch 9)

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    name                  "Chris Ryan"
-    email                 "cryan@nearinfinity.com"
+    #name                  "Chris Ryan"
+    #email                 "cryan@nearinfinity.com"
+    sequence(:name)       { |n| "Person #{n}" }
+    sequence(:email)      { |n| "person_#{n}@example.com"}
     password              "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
